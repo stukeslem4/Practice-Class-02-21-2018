469f35b7bf809ad7344e681049d1f94c0ea15f56
Lemon <s.lemon.2016@mymail.jcsu.edu>
2018-02-21 13:22:34 -0500

    second commit

---
+++ PersonTest.java
@@ -0,0 +1,25 @@
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+Person p=new Person("Jay", 17);
+
+
+System.out.println(p.isAdult());
+		
+		
+		
+		
+		
+		
+		
+	}
+
+	
+	
+	
+	
+}
